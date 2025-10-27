0b35aaa2a7fc352b3fc140a2d1542d845d26e3d6
Seshu <scharla@purdue.edu>
2025-10-27 11:21:52 -0500

    Made use fo the simulation data

---
+++ SatDetection/NecessaryCondDemo.py
@@ -0,0 +1,40 @@
+import numpy as np
+import cvxpy as cp
+from DataProcessing.SimData import filt_data
+from DataProcessing.SimData import unit_convs as uc
+from SatDetection import SatParmID as spd
+import matplotlib.pyplot as plt
+# ==============================================
+
+sim_data = filt_data.load_filtered_sim_data_set()
+# tst_data = decimate_data.load_decimated_test_data_set()
+
+thetas = np.zeros([3, 3])
+for i in range(3):
+        thetas[i,:] = spd.solve_LP(sim_data[i].ssd['eta'], sim_data[i].ssd['T'], sim_data[i].ssd['F'], sim_data[i].ssd['u1']).T
+
+labels = [r'Nominal urea dosing', r'$+20\%$ urea dosing', r'$-20\%$ urea dosing']
+# Plotting response
+for i in range(3):
+        plt.figure(0)
+        plt.plot(sim_data[i].ssd['t'], sim_data[i].ssd['eta'], label=labels[i])
+        plt.figure(1)
+        plt.plot(sim_data[i].ssd['t'], spd.pred_sat_response(np.matrix(thetas[i,:]).T, sim_data[i].ssd['T'],
+                                                             sim_data[i].ssd['F'], sim_data[i].ssd['u1']) ,
+                 label=labels[i])
+plt.figure(0)
+plt.grid()
+plt.legend()
+plt.ylabel(r'$\eta$ '+uc.units['eta'])
+plt.xlabel("Time [s]")
+plt.title(r'RMC Test $NO_x$ Reduction with $\pm 20$ Urea Dosing Gain Variation')
+plt.savefig("./SatDetection/figs/eta_sim.eps")
+plt.figure(1)
+plt.grid()
+plt.legend()
+plt.ylabel(r'$\eta_{sat}$ '+uc.units['eta'])
+plt.xlabel("Time [s]")
+plt.title(r'Predicted $NO_x$ Reducion Under Catalyst Saturation')
+plt.savefig("./SatDetection/figs/eta_sat.eps")
+
+plt.show()
--- SatDetection/SatParmID.py
+++ SatDetection/SatParmID.py
@@ -1,7 +1,48 @@
 # Saturated System Parameter Identification
 import numpy as np
 import cvxpy as cp
+# ===========================================
 
-class SatParm:
-        """Saturated System Parameter Estimation"""
-        def __init__(self, data)
+def PhiSat_mat(T, F, u1):
+        """Returns the regression matrix for the given series or T, F and u1
+           eta[k+1] = (u1[k]/F[k]) * [T**2 T 1] * [th1, th2 th3]^T
+        """
+        N = len(T)
+        PhiSat = np.zeros([N, 3])
+        # Looping
+        for i in range(N):
+                PhiSat[i, :] = (u1[i]/F[i]) * np.array([T[i]**2, T[i], 1])
+        #===
+        return PhiSat
+# ===============================================================================
+
+def solve_LP(eta, T, F, u1):
+        """ Solves the bounding linear programming problem for the given time-series data """
+        # Linear programming problem
+        Phi_sat = PhiSat_mat(T, F, u1)
+        eta_mat = np.matrix(eta).T
+        theta = cp.Variable([3, 1])
+        problem = cp.Problem(cp.Minimize(cp.sum(Phi_sat[:-1, :] @ theta)),
+                                [
+                                        Phi_sat[:-1, :] @ theta >= eta_mat[1:, :],      #\eta[k+1] <= \phi[k]^T \theta
+                                        theta[0, 0] <= 0,
+                                        theta[2, 0] >= 0
+                                ]
+                        )
+        problem.solve()
+        return np.matrix(theta.value)
+# =====================================================================================================================
+
+
+def pred_sat_response(theta, T, F, u1):
+        """ Predicted saturated systems response in time an returns array of eta_sat """
+        phi_sat = PhiSat_mat(T, F, u1)
+        eta_sat_hat = np.zeros(np.shape(T))
+        eta_sat_hat[1:] = (phi_sat[0:-1, :] @ theta).flatten()
+        eta_sat_hat[0] = eta_sat_hat[1]
+        return eta_sat_hat
+
+
+
+# class SatParm:
+        # """Saturated System Parameter Estimation"""
